e68db303ef35e9ad93d7b20d3e60ddb215ad645a
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-03-14 12:21:21 +0000

    find role, department, employee

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const inquirer = require('inquirer');
+require('console.table');
 const mysql = require('mysql2');
 
 const db = mysql.createConnection(
@@ -22,11 +23,17 @@ const db = mysql.createConnection(
         {
             type: 'list',
             message: 'What would you like to do?',
-            choices: ["View All Employees", "Add An Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"],
+            choices: ["View All Departments", "View All Roles", "View All Employees", "Add An Employee", "Update Employee Role",  "Add Role", "Add Department"],
             name: 'firstInquiry' 
         }
     ]).then((response) => {
         switch (response.firstInquiry) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles' :
+                viewAllRoles();
+                break;
             case 'View All Employees':
                 viewAllEmployees();
                 break;
@@ -36,25 +43,48 @@ const db = mysql.createConnection(
             case 'Update Employee Role':
                 updateEmployeeRole();
                 break;
-            case 'View All Roles' :
-                viewAllRoles();
-                break;
             case 'Add Role':
                 addRole();
-                break;
-            case 'View All Departments':
-                viewAllDepartments();
                 break;
             case 'Add Department':
                 addDepartment();
                 break;
         }
     })
-  }
+  };
 
-  viewAllEmployees = () => {
-    db.query('SELECT id, first_name, last_name FROM employee', (err, results) => {
-        console.table(results);
+viewAllDepartments = () => {
+    const query = `SELECT * FROM department ORDER BY id ASC;`;
+    db.query(query, (err, results) => {
+        if (err) throw err;
+        console.table('\n', results, '\n');
         init();
     })
 };
+
+viewAllRoles = () => {
+    const query = `SELECT role.id, role.title, role.salary, department.name AS department, department.id
+    FROM role
+    JOIN department ON role.department_id = department.id
+    ORDER BY role.id ASC;`;
+    db.query(query, (err, results) => {
+    if (err) throw err;
+    console.table('\n', results, '\n');
+    init();
+    })
+};
+
+  viewAllEmployees = () => {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`;
+    db.query(query, (err, results) => {
+        if (err) throw err;
+        console.table('\n', results, '\n');
+        init();
+    })
+};
+
