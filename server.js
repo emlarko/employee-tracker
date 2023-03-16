fec46e3191134f5a527107ae8485cee4ef0532da
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-03-16 11:38:07 +0000

    update manager, dept salary

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = mysql.createConnection(
         {
             type: 'list',
             message: 'What would you like to do?',
-            choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add An Employee", "Update Employee Role"],
+            choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add An Employee", "Update Employee Role", "Update Employee's Manager", "View the total utilized budget of a department"],
             name: 'firstInquiry' 
         }
     ]).then((response) => {
@@ -48,6 +48,12 @@ const db = mysql.createConnection(
                 break;
             case 'Update Employee Role':
                 updateEmployeeRole();
+                break;
+            case "Update Employee's Manager":
+                updateEmployeeManager();
+                break;
+            case 'View the total utilized budget of a department':
+                viewDeptSalary();
                 break;
         }
     })
@@ -103,17 +109,17 @@ addDepartment = () => {
         },
         (err, result) => {
             if (err) throw err;
-            console.table(`\n ${response.newDept} successfully added to database! \n`);
+            console.log(`\n ${response.newDept} successfully added to database! \n`);
             init();
         })
     })
 };
 
 addRole = () => {
-    let query = `SELECT * FROM department;`
+    let query = `SELECT * FROM department;`;
     db.query(query, (err, results) => {
         if (err) throw err;
-        let departments = results.map(department => ({name: department.name, value: department.id }));
+        let departments = results.map(department => ({name: department.name, value: department.id}));
         inquirer.prompt([
             {
                 type: 'input',
@@ -130,7 +136,7 @@ addRole = () => {
                 message: 'Which department do you want to add the new role to?', 
                 choices: departments,
                 name: 'deptName'
-            },
+            }
         ]).then((response) => {
             let query = `INSERT INTO role SET ?`
             db.query(query, 
@@ -139,7 +145,7 @@ addRole = () => {
                 salary: response.salary,
                 department_id: response.deptName,
             },
-            (err, res) => {
+            (err, result) => {
                 if (err) throw err;
                 console.log(`\n ${response.title} successfully added to database! \n`);
                 init();
@@ -147,3 +153,157 @@ addRole = () => {
         })
     })
 };
+
+addAnEmployee = () => {
+    let query = `SELECT * FROM role;`;
+    db.query(query, (err, results) => {
+        if (err) throw err;
+        let roles = results.map(role => ({name: role.title, value: role.id}));
+            let query = `SELECT * FROM employee`;
+            db.query(query, (err, results) => {
+                if (err) throw err;
+                let employees = results.map(employee => ({name: employee.first_name + ' ' + employee.last_name, value: employee.id}));
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: "What is the employee's first name", 
+                name: 'firstName'
+            },
+            {
+                type: 'input',
+                message: "What is the employee's last name?", 
+                name: 'lastName' 
+            },
+            {
+                type: 'list',
+                message: "What is the employee's title?", 
+                choices: roles,
+                name: 'role'
+            },
+            {
+                type: 'list',
+                message: "Who is the employee's manager?", 
+                choices: employees,
+                name: 'manager'
+            }
+        ]).then((response) => {
+            let query = `INSERT INTO employee SET ?`;
+            db.query(query,
+                {
+                    first_name: response.firstName,
+                    last_name: response.lastName,
+                    role_id: response.role,
+                    manager_id: response.manager
+                },(err, result) => {
+                    if (err) throw err;
+                    console.log(`\n ${response.firstName} ${response.lastName} successfully added to database! \n`);
+                    init();
+                })
+            })
+        })
+    })
+};
+
+updateEmployeeRole = () => {
+    let query = `SELECT * FROM role;`;
+    db.query(query, (err, results) => {
+        if (err) throw err;
+        let roles = results.map(role => ({name: role.title, value: role.id}));
+            let query = `SELECT * FROM employee`;
+            db.query(query, (err, results) => {
+                if (err) throw err;
+                let employees = results.map(employee => ({name: employee.first_name + ' ' + employee.last_name, value: employee.id}));
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: "What is the employee's name?", 
+                choices: employees,
+                name: 'employee'
+            },
+            {
+                type: 'list',
+                message: "What is the employee's new role?", 
+                choices: roles,
+                name: 'newRole'
+            }
+        ]).then((response) => {
+            console.log(response.employee);
+            console.log(response.newRole);
+            let query = `UPDATE employee SET ? WHERE ?`;
+            db.query(query,
+                [{
+                    role_id: response.newRole,
+                },
+                {
+                    id: response.employee,
+                }],(err, result) => {
+                    if (err) throw err;
+                    console.log(`\n Successfully updated employee's role \n`);
+                    init();
+                })
+            })
+        })
+    })
+};
+
+updateEmployeeManager = () => {
+    let query = `SELECT * FROM employee`;
+        db.query(query, (err, results) => {
+            if (err) throw err;
+            let employees = results.map(employee => ({name: employee.first_name + ' ' + employee.last_name, value: employee.id}));
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: "What is the employee's name?", 
+                choices: employees,
+                name: 'employee'
+            },
+            {
+                type: 'list',
+                message: "Who is the employee's new manager?", 
+                choices: employees,
+                name: 'manager'
+            }
+        ]).then((response) => {
+            let query = `UPDATE employee SET ? WHERE ?`;
+            db.query(query,
+                [{
+                    manager_id: response.manager,
+                },
+                {
+                    id: response.employee,
+                }],(err, result) => {
+                    if (err) throw err;
+                    console.log(`\n Successfully updated employee's manager \n`);
+                    init();
+                })
+         })
+    })
+};
+
+viewDeptSalary = () => {
+    const query = `SELECT * FROM department ORDER BY id ASC;`;
+    db.query(query, (err, results) => {
+        let departments = results.map(department => ({name: department.name, value: department.id}));
+        if (err) throw err;
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Which Department would you like to view?', 
+            choices: departments,
+            name: 'deptName'
+        }
+    ]).then((response) => {
+        let query = `SELECT SUM(salary) AS total_salary FROM role WHERE ?`;
+        db.query(query,
+            {
+                department_id: response.deptName,
+            }, (err, results) => {
+            if (err) throw err;
+            console.log(`\n The total utilized budget of ${response.deptName} is \n`);
+            console.table(results);
+            })
+        })
+    })
+};
+
